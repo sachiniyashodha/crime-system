24001993f0bd7cb020fc32725ab6ab6091cf3272
thilina19 <tnarade@gmail.com>
2020-11-26 21:27:37 +0530

    Add data form fixing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,37 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="form -group row">
-        <div class="col-md-8 col-mx-auto">
-        <form class= "form-horizontal custm-form" method="post" action="">
-            
-        <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crime_management_system</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <link rel="stylesheet" href="assets/css/Google-Style-Login.css">
-    <link rel="stylesheet" href="assets/css/Pretty-Header.css">
-    <link rel="stylesheet" href="assets/css/Hero-Technology.css">
-    <link rel="stylesheet" href="assets/css/Pretty-Registration-Form.css">
-</head>
 
-<body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header"><a class="navbar-brand navbar-link" href="#">Refersh </a>
-                <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-            </div>
-            <div class="collapse navbar-collapse" id="navcol-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li role="presentation"><a href="index.html">LogOut </a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
     <div class="jumbotron hero-technology">
         <h1 class="hero-title"><strong>CRIME MANAGEMENT SYSTEM</strong></h1>
         <p class="text-muted"><a class="btn btn-primary btn-lg hero-button" role="button" href="main.html">ENTER TO DATABASE</a></p>
@@ -62,10 +32,5 @@
             <div>Someone famous</div>
         </blockquote>
     </div>
-    <div class="col-md-12">
-        <h4 class="text-center">CRIME MANAGEMENT SYSTEM- 2020 </h4></div>
-    <h4 class="text-center"> </h4>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-</body>
+
 @endsection
