c8f4444fcfe75476c0427197866e4a338456d64d
thilina19 <tnarade@gmail.com>
2020-11-26 21:26:25 +0530

    New View fixing

--- resources/views/fir.blade.php
+++ resources/views/fir.blade.php
@@ -12,7 +12,8 @@
                         </div>
                         <div class="card-body pt-5 ">
 
-                    <form class="form-horizontal custom-form" method="POST" action="{{ route('crime_files_form') }}">
+                    <form class="form-horizontal custom-form" >
+{{--                        method="POST" action="{{ route('crime_files_form') }}"--}}
                     {{ csrf_field() }}
                     <div class="row">
                     <div class="col-md-12">
@@ -175,7 +176,8 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <form name="delete_crime_files_form" method="POST" action="{{ route('delete_fir') }}">
+                <form name="delete_crime_files_form" >
+{{--                    method="POST" action="{{ route('delete_fir') }}"--}}
                     {{ csrf_field() }}
                     <input type="hidden" name="delete_fir_id" id="delete_fir_id" >
                     <div class="modal-body">
