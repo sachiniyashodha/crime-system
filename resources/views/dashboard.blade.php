4f4b1227758b02b996b8655949cdfdd4e15ab144
thilina19 <tnarade@gmail.com>
2020-12-03 00:37:46 +0530

    Change color main section

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="card">
-        <div class="card-body pt-5 ">
+    <div class="card card-img">
+        <div class="card-body  pt-5 ">
             <div class="row">
                 <div class="col-md-12 text-center">
-                    <div class="jumbotron hero-technology ">
+                    <div class="jumbotron hero-technology jumbotron-black">
                         <h1 class="hero-title"><strong>CRIME MANAGEMENT SYSTEM </strong></h1>
-                        <h2>Speeds up Real Time Crime Traking. </h2>
-                        <h2>User Experience Focused. </h2>
-                        <h2>Easy to work with Database. </h2>
+                        <h2 class="text-white">Speeds up Real Time Crime Traking. </h2>
+                        <h2 class="text-white">User Experience Focused. </h2>
+                        <h2 class="text-white">Easy to work with Database. </h2>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-12 text-center">
-                    <p>The need for good record-keeping and information sharing practices has taken on added significance in today's global environment. Not only do good records provide crucial internal information (i.e.,business operations and case management support-not
+                    <p class="text-white colour-effect">The need for good record-keeping and information sharing practices has taken on added significance in today's global environment. Not only do good records provide crucial internal information (i.e.,business operations and case management support-not
                         to mention the official memory of an agency's investigations), law enforcement agencies now need to communicate agency-to-agency and across continents in order to protect the Nation's citizens. Nothing is more important to accomplishing that
                         mission than having accessibility to accurate and timely records. Calls for service records and investigative, arrest, criminal identification, detention, and even civil records hold information that by themselves mean little; however, when
                         pieced together with information rom other jurisdictions, the result can help with all Nation levels of investigations and aid in safeguarding the nation. Crime Record Management system applies to all police stations across the country and
@@ -28,49 +28,50 @@
     <div class="row">
         <div class="col-md-4">
             <div class="card">
-                <div class="card-body pt-5 ">
-                    <h1 class="text-center">ABOUT US</h1>
-                    <p>Crime management system for Puttalam police district. Initially, it focuses on Puttalam district area police stations to formulate a method for supportingactivities in the police department. The police offices can be used this web-based solution
+                <div class="bg-color p-5">
+                    <h1 class="text-center text-white"><strong> ABOUT US </strong></h1>
+                    <hr>
+                    <p class="text-white font-weight-bold">Crime management system for Puttalam police district. Initially, it focuses on Puttalam district area police stations to formulate a method for supportingactivities in the police department. The police offices can be used this web-based solution
                         in addition to their manual process. Then, his project aims to automate the current process. </p>
-                    <p>Crime management system for Puttalam police district. Initially, it focuses on Puttalam district area police stations to formulate a method for supportingactivities in the police department. The police offices can be used this web-based solution
+                    <p class="text-white font-weight-bold">Crime management system for Puttalam police district. Initially, it focuses on Puttalam district area police stations to formulate a method for supportingactivities in the police department. The police offices can be used this web-based solution
                         in addition to their manual process. Then, his project aims to automate the current process. </p>
-                    <p>Crime management system for Puttalam police district. Initially, it focuses on Puttalam district area police stations to formulate a method for supportingactivities in the police department. The police offices can be used this web-based solution
+                    <p class="text-white font-weight-bold">Crime management system for Puttalam police district. Initially, it focuses on Puttalam district area police stations to formulate a method for supportingactivities in the police department. The police offices can be used this web-based solution
                         in addition to their manual process. Then, his project aims to automate the current process. </p>
                 </div>
             </div>
         </div>
         <div class="col-md-4">
             <div class="card">
-                <div class="card-body pt-5 ">
+                <div class="card-body bg-color">
                     <div class="row">
-                        <div class="">
-                            <h1 class="text-center">FUNCTIONS</h1>
-                            <h4 class="text-left">Data Entry in Easy Manner. </h4>
-                            <h4 class="text-left">Criminal's List Across All Over The Nation. </h4>
-                             <h4 class="text-left">Valuable Officer's List. </h4>
-                            <h4 class="text-left">Tracking of Crimes In Different City. </h4>
-                            <h4 class="text-left">Real-time Case Progress Analysis. </h4>
+                        <div class="p-5">
+                            <h1 class="text-center text-white"><strong> FUNCTIONS </strong></h1>
+                            <hr>
+                            <h4 class="text-left text-white font-weight-bold">Data Entry in Easy Manner. </h4>
+                            <h4 class="text-left text-white font-weight-bold">Criminal's List Across All Over The Nation. </h4>
+                            <h4 class="text-left text-white font-weight-bold">Valuable Officer's List. </h4>
+                            <h4 class="text-left text-white font-weight-bold">Tracking of Crimes In Different City. </h4>
+                            <h4 class="text-left text-white font-weight-bold">Real-time Case Progress Analysis. </h4>
                         </div>
                     </div>
-                    <div class="row">
-                        <hr>
-                        
-                        
-                    </div>
                 </div>
             </div>
         </div>
         <div class="col-md-4">
             <div class="card">
-                <div class="card-body pt-5 ">
-                    <h1 class="text-center">POLICE NEWS</h1><img src="{{ asset('img/criminalimg.png') }}">
+                <div class="card-body bg-color p-5 ">
+                    <h1 class="text-center text-white"><strong> POLICE NEWS </strong></h1>
+                    <div class="text-center">
+                        <img class="mx-auto" src="{{ asset('img/criminalimg.png') }}">
+                    </div>
+                    <hr>
                     <blockquote>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                        <div>Someone famous</div>
+                        <p class="text-white font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                        <div class="text-white font-weight-bold">Someone famous</div>
                     </blockquote>
                     <blockquote>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                        <div>Someone famous</div>
+                        <p class="text-white font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                        <div class="text-white font-weight-bold">Someone famous</div>
                     </blockquote>
                 </div>
             </div>
